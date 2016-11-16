3fa74763918afadca2630b31a04a8501783964fb
rdhupia <rsdhupia@gmail.com>
2016-11-16 09:38:54 -0500

    Changed statuses, drag and drop for all milestones tasks

--- js/ui.js
+++ js/ui.js
@@ -42,5 +42,187 @@ $(document).ready(function() {
 	});
 	
 	
+	$('.usersAssigned7').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned7').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned8').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned8').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned9').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned9').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned10').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned10').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned11').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned11').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned12').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned12').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned13').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned13').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned14').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned14').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned15').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned15').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned16').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned16').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned17').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned17').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned18').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned18').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned19').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned19').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned20').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned20').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned21').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned21').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned22').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned22').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned23').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned23').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned24').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned24').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned25').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned25').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned26').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned26').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned27').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned27').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned28').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned28').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned29').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned29').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned30').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned30').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned31').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned31').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned32').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned32').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned33').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned33').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned34').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned34').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned35').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned35').append(contents + '<br />');
+	}
+	});
+	
+	$('.usersAssigned36').droppable({ hoverClass: 'border', accept: '.users',
+	drop: function() {
+		$('.usersAssigned36').append(contents + '<br />');
+	}
+	});
+	
+	
+	
 	
 });
